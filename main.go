5897a649bbc54d966f6eee43f7efefb6bb0d3659
Hadjibaev Kahramon <hp@oilnew.ru>
2023-04-01 21:33:07 +0300

    Create files

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// _____ If else example _____
+
+	// _____ Switch example _____
+
+	// _____  For Loop example:
+
+	// _____  For Loop with Condition example:
+
+	// _____  Continue & Break example:
+
+	// _____  Struct example:
+
+	// _____  Class example:
+
+}
